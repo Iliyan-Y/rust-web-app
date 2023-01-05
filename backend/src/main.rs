550a6a1759c9fccf9e682476f285aeab379694eb
Iliyan-Y <maximizebg@gmail.com>
2023-01-05 12:56:24 +0000

    get + state

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,11 @@
 use dotenv;
-use sqlx::{postgres::PgPoolOptions, query};
-use tide;
+use sqlx::{postgres::PgPoolOptions, query, PgPool};
+use tide::{self, Request, Server};
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
   dotenv::dotenv().ok();
   pretty_env_logger::init();
-  let mut app = tide::new();
 
   // setup db
   let db_url = std::env::var("DATABASE_URL")?;
@@ -14,14 +13,29 @@ async fn main() -> Result<(), Error> {
     .max_connections(5)
     .connect(&db_url)
     .await?;
-  // assert db is connected
-  let rows = query!("select 1 as one").fetch_one(&db_pool).await?;
-  dbg!(rows);
 
-  app.at("/").get(|_| async move { Ok("HELLO WORLD") });
+  // Create the server with db pool as a state
+  let mut app = Server::with_state(State { db_pool });
+  app.at("/").get(|req: Request<State>| async move {
+    let db_pool_from_state = &req.state().db_pool;
+
+    // assert db is connected
+    let rows = query!("select 1 as one")
+      .fetch_one(db_pool_from_state)
+      .await?;
+    dbg!(rows);
+    Ok("HELLO WORLD")
+  });
   app.listen("localhost:3000").await?;
 
   Ok(())
+}
+
+// SERVER STATE is shared across all handlers
+// it need to be static
+#[derive(Debug, Clone)]
+struct State {
+  db_pool: PgPool,
 }
 
 #[derive(thiserror::Error, Debug)]
