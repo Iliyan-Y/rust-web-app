06b932b6970d5f1c41de69234909a49289678f00
Iliyan-Y <maximizebg@gmail.com>
2023-01-05 12:30:07 +0000

    db connection created

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,22 @@
 use dotenv;
+use sqlx::{postgres::PgPoolOptions, query};
 use tide;
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
   dotenv::dotenv().ok();
+  pretty_env_logger::init();
   let mut app = tide::new();
-  let db_url = std::env::var("DATABASE_URL").unwrap();
+
+  // setup db
+  let db_url = std::env::var("DATABASE_URL")?;
+  let db_pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&db_url)
+    .await?;
+  // assert db is connected
+  let rows = query!("select 1 as one").fetch_one(&db_pool).await?;
+  dbg!(rows);
 
   app.at("/").get(|_| async move { Ok("HELLO WORLD") });
   app.listen("localhost:3000").await?;
@@ -20,4 +31,6 @@ enum Error {
 
   #[error(transparent)]
   IoError(#[from] std::io::Error),
+  #[error(transparent)]
+  VarError(#[from] std::env::VarError),
 }
