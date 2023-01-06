9b6bd6a57dd3360d9c3b8fc42f7d712744027e01
Iliyan-Y <maximizebg@gmail.com>
2023-01-06 10:02:06 +0000

    returns json

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,34 +1,36 @@
 use dotenv;
+use serde_json::json;
 use sqlx::{postgres::PgPoolOptions, query, PgPool};
-use tide::{self, Request, Server};
+use tide::{self, Body, Request, Response, Server, StatusCode};
 
 #[async_std::main]
-async fn main() -> Result<(), Error> {
+async fn main() {
   dotenv::dotenv().ok();
   pretty_env_logger::init();
 
   // setup db
-  let db_url = std::env::var("DATABASE_URL")?;
+  let db_url = std::env::var("DATABASE_URL").unwrap();
   let db_pool = PgPoolOptions::new()
     .max_connections(5)
     .connect(&db_url)
-    .await?;
+    .await
+    .unwrap();
 
   // Create the server with db pool as a state
   let mut app = Server::with_state(State { db_pool });
   app.at("/").get(|req: Request<State>| async move {
-    let db_pool_from_state = &req.state().db_pool;
-
+    // let db_pool_from_state = &req.state().db_pool;
     // assert db is connected
-    let rows = query!("select 1 as one")
-      .fetch_one(db_pool_from_state)
-      .await?;
-    dbg!(rows);
-    Ok("HELLO WORLD")
-  });
-  app.listen("localhost:3000").await?;
+    // let rows = query!("select 1 as one")
+    //   .fetch_one(db_pool_from_state)
+    //   .await?;
+    // dbg!(rows);
+    let some_json = json!([1, 2, 3, 4]);
 
-  Ok(())
+    // let body = Body::from_json(&some_json).unwrap();
+    Ok(some_json)
+  });
+  app.listen("localhost:3000").await.unwrap();
 }
 
 // SERVER STATE is shared across all handlers
